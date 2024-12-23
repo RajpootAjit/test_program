c8386635a956e9aa73ac690c19d7c3f531247aec
AjitSingh <ajitsingh@Ajits-MacBook-Air.local>
2024-12-23 23:22:22 +0530

    Add new program: Description of the changes

---
+++ src/test/java/com/java/program/Add_Two_Number.java
@@ -0,0 +1,17 @@
+package com.java.program;
+
+public class Add_Two_Number {
+	
+	public static int add(int num1, int num2) {
+		int sum = num1 + num2;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(add(2,3));
+
+	}
+
+}
